86f20fc39c1ea5ed8c55615e60d014654434b0e3
tabby <tabby@tabbys-air.home>
2024-09-20 08:37:43 +0200

    20-09-2024_0 Alpha:

--- assets/shaders/vulkan/test.glsl
+++ assets/shaders/vulkan/test.glsl
@@ -1,6 +1,9 @@
 #pragma lang : glsl
 #pragma stage : vertex
 #version 450
+layout(binding = 0) uniform UniformBufferObject {
+    vec2 position;
+} ubo;
 
 layout(location = 0) in vec2 inPosition;
 layout(location = 1) in vec3 inColor;
@@ -8,7 +11,7 @@ layout(location = 1) in vec3 inColor;
 layout(location = 0) out vec3 fragColor;
 
 void main() {
-    gl_Position = vec4(inPosition, 0.0, 1.0);
+    gl_Position = vec4(ubo.position + inPosition, 0.0, 1.0);
     fragColor = inColor;
 }
 
