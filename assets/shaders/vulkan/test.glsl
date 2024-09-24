3d40b151df5660dd2f503350876a148c38cf4713
tabby <tabby@tabbys-air.home>
2024-09-24 22:39:27 +0200

    24-09-2024_1 Alpha: Vulkan Image(broken)

--- assets/shaders/vulkan/test.glsl
+++ assets/shaders/vulkan/test.glsl
@@ -7,20 +7,26 @@ layout(binding = 0) uniform UniformBufferObject {
 
 layout(location = 0) in vec2 inPosition;
 layout(location = 1) in vec3 inColor;
+layout(location = 2) in vec2 inTexCoord;
 
 layout(location = 0) out vec3 fragColor;
+layout(location = 1) out vec2 fragTexCoord;
 
 void main() {
     gl_Position = vec4(ubo.position + inPosition, 0.0, 1.0);
     fragColor = inColor;
+    fragTexCoord = inTexCoord;
 }
 
 #pragma stage : fragment
 #version 450
+layout(binding = 1) uniform sampler2D texSampler;
 
 layout(location = 0) in vec3 fragColor;
+layout(location = 1) in vec2 fragTexCoord;
+
 layout(location = 0) out vec4 outColor;
 
 void main() {
-    outColor = vec4(fragColor, 1.0);
+    outColor = texture(texSampler, fragTexCoord);
 }
