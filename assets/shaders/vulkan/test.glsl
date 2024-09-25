ec870e187e4bb16c354affcbb44a467ffc088011
tabdroid-1 <tabdroid.official@gmail.com>
2024-09-25 21:29:26 +0200

    25-09-2024_1 Alpha:

--- assets/shaders/vulkan/test.glsl
+++ assets/shaders/vulkan/test.glsl
@@ -2,10 +2,12 @@
 #pragma stage : vertex
 #version 450
 layout(binding = 0) uniform UniformBufferObject {
-    vec2 position;
+    mat4 model;
+    mat4 view;
+    mat4 proj;
 } ubo;
 
-layout(location = 0) in vec2 inPosition;
+layout(location = 0) in vec3 inPosition;
 layout(location = 1) in vec3 inColor;
 layout(location = 2) in vec2 inTexCoord;
 
@@ -13,7 +15,7 @@ layout(location = 0) out vec3 fragColor;
 layout(location = 1) out vec2 fragTexCoord;
 
 void main() {
-    gl_Position = vec4(ubo.position + inPosition, 0.0, 1.0);
+    gl_Position = ubo.proj * ubo.view * ubo.model * vec4(inPosition, 1.0);
     fragColor = inColor;
     fragTexCoord = inTexCoord;
 }
